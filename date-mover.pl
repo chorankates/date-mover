88001196ca93b072b9ee86655398509eb6bee853
Conor Horan-Kates <conor.code@gmail.com>
2011-07-12 00:00:40 -0700

    functional commit.. hardly seems worth it

--- date-mover.pl
+++ date-mover.pl
@@ -5,8 +5,10 @@ use strict;
 use warnings;
 
 use Cwd;
+use File::Find; # not actually using this yet, but should
 use File::Basename;
 use File::Spec;
+
 
 my %s = (
 	home => Cwd::getcwd(),
@@ -14,5 +16,27 @@ my %s = (
 	verbose => 1,
 );
 
+my @files = glob(File::Spec->catfile($s{home},'*'));
+
+for my $file (@files) {
+	next unless -f $file;
+	# determine date of last modification
+	my $fname = basename($file);
+	my $modified_time = (stat($file))[9]; # 7 is created, 8 is accessed
+	my @localtime = localtime($modified_time);
+	
+	my $year = $localtime[5] + 1900;
+	my $month = $localtime[4] + 1;
+	my $day = $localtime[3];
+	
+	my $new_path = join('-', ($year, $month, $day));
+	my $new_file = File::Spec->catdir($s{home}, $new_path, $fname);
+	
+	my $cmd = "mv $file $new_file";
+	
+	my $results = system($cmd);
+	
+	warn "WARN:: unable to move '$file': $results" if $results;
+}
 
 exit;
