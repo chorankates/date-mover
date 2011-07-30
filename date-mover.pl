9974b1770f0988050f703be9917eced1be7dac5f
Conor Horan-Kates <conor.code@gmail.com>
2011-07-30 11:39:54 -0700

    plane work SFO->BOS

--- date-mover.pl
+++ date-mover.pl
@@ -1,11 +1,11 @@
 #!/usr/bin/perl -w
-## date-mover.pl - move files into subdirectories based on their last modification time
+## date-mover.pl - move files into subdirectories based on accessed/created/modified time
 
 use strict;
 use warnings;
 
 use Cwd;
-use File::Find; # not actually using this yet, but should
+use File::Find; # not actually using this yet, but should <-- how do we want to handle recursion? should previously moved files be moved again if their mtime changes?
 use File::Basename;
 use File::Spec;
 
@@ -13,9 +13,15 @@ use File::Spec;
 my %s = (
 	home => Cwd::getcwd(),
 	
+	time => 'modified', # allow modified / accessed / created
+	
 	verbose => 1,
 );
 
+$s{time}  = ($s{time} eq 'created') ? 7 :
+                     ($s{time} eq 'accessed') ? 8 :
+					 9;
+					 
 $s{home} = shift @ARGV if -d $ARGV[0];
 my @files = glob(File::Spec->catfile($s{home},'*'));
 
@@ -23,9 +29,9 @@ for my $file (@files) {
 	next unless -f $file;
 	# determine date of last modification
 	my $fname = basename($file);
-	my $modified_time = (stat($file))[9]; # 7 is created, 8 is accessed
-	my @localtime = localtime($modified_time);
-	
+	my $key_time = (stat($file))[$s{time}];
+	my @localtime = localtime($key_time);
+
 	my $year = $localtime[5] + 1900;
 	my $month = $localtime[4] + 1;
 	my $day = $localtime[3];
